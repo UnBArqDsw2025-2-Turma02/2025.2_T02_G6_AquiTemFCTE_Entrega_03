7a79bdb7d1daa30be96f6338f44d12516732ca0c
IgorJustino <211061897@aluno.unb.br>
2025-10-12 22:49:31 -0300

    feat(builder): Gerenciador-produtoBuilder-service

--- GOFs/Criacionais/Builder/ProdutoBuilder.ts
+++ GOFs/Criacionais/Builder/ProdutoBuilder.ts
@@ -1,16 +1,47 @@
-// import Produto from "./Produto";
+import Produto from "./Produto";
 
-// interface ProdutoVenda { 
-//   protected produto: Produto;
+export class ProdutoBuilder {
+  protected produto?: Produto;
 
-//   getProduto(): Produto {
-//     return this.produto;
-//   }
+  criarNovoProduto(nome: string, descricao: string): this {
+    this.produto = new Produto(nome, descricao);
+    return this;
+  }
 
-//   criarNovoProduto(nome: string, descricao: string): void {
-//     this.produto = new Produto(nome, descricao);
-//   }
+  buildImagem(foto: string): this {
+    if (!this.produto) throw new Error("Chame criarNovoProduto primeiro");
+    this.produto.setImagemProduto(foto);
+    return this;
+  }
 
-//   abstract buildVendido(): void;
-//   abstract buildImagem(foto: string): void;
-// }
+  buildPreco(preco: number): this {
+    if (!this.produto) throw new Error("Chame criarNovoProduto primeiro");
+    this.produto.setPrecoProduto(preco);
+    return this;
+  }
+
+  buildCategoria(categoria: string): this {
+    if (!this.produto) throw new Error("Chame criarNovoProduto primeiro");
+    this.produto.setCategoriaProduto(categoria);
+    return this;
+  }
+
+  buildVendido(): this {
+    if (!this.produto) throw new Error("Chame criarNovoProduto primeiro");
+    this.produto.setVendido();
+    return this;
+  }
+
+  buildModalidade(modalidade: string): this {
+    if (!this.produto) throw new Error("Chame criarNovoProduto primeiro");
+    this.produto.setModalidadeProduto(modalidade);
+    return this;
+  }
+
+  getProduto(): Produto {
+    if (!this.produto) throw new Error("Produto não foi criado");
+    const result = this.produto;
+    this.produto = undefined;
+    return result;
+  }
+}
